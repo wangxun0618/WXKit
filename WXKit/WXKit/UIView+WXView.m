4180e4aede06ae89c8e56f1682f501f0c433b5a2
wangxun0618 <simonxun@163.com>
2018-08-02 11:04:26 +0800

    Update UIView+WXView.m

--- WXKit/WXKit/UIView+WXView.m
+++ WXKit/WXKit/UIView+WXView.m
@@ -379,6 +379,44 @@ static char kActionHandlerLongPressGestureKey;
     return result;
 }
 
+
+#pragma mark - layer
+- (void)rounded:(CGFloat)cornerRadius {
+    [self rounded:cornerRadius width:0 color:nil];
+}
+
+- (void)border:(CGFloat)borderWidth color:(UIColor *)borderColor {
+    [self rounded:0 width:borderWidth color:borderColor];
+}
+
+- (void)rounded:(CGFloat)cornerRadius width:(CGFloat)borderWidth color:(UIColor *)borderColor {
+    self.layer.cornerRadius = cornerRadius;
+    self.layer.borderWidth = borderWidth;
+    self.layer.borderColor = [borderColor CGColor];
+    self.layer.masksToBounds = YES;
+}
+
+
+-(void)round:(CGFloat)cornerRadius RectCorners:(UIRectCorner)rectCorner {
+    
+    UIBezierPath *maskPath = [UIBezierPath bezierPathWithRoundedRect:self.bounds byRoundingCorners:rectCorner cornerRadii:CGSizeMake(cornerRadius, cornerRadius)];
+    CAShapeLayer *maskLayer = [[CAShapeLayer alloc] init];
+    maskLayer.frame = self.bounds;
+    maskLayer.path = maskPath.CGPath;
+    self.layer.mask = maskLayer;
+}
+
+
+-(void)shadow:(UIColor *)shadowColor opacity:(CGFloat)opacity radius:(CGFloat)radius offset:(CGSize)offset {
+    //给Cell设置阴影效果
+    self.layer.masksToBounds = NO;
+    self.layer.shadowColor = shadowColor.CGColor;
+    self.layer.shadowOpacity = opacity;
+    self.layer.shadowRadius = radius;
+    self.layer.shadowOffset = offset;
+}
+
+
 #pragma mark -- 添加边框
 - (UIView *)borderForColor:(UIColor *)color
                borderWidth:(CGFloat)borderWidth
